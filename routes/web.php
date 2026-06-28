d2776524e2b34d7bc54d3c4d8358ee7f1a1f5c99
greya734 <rosajoshu@gmail.com>
2026-06-28 21:17:14 +1100

    Update web.php
    config route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
-<?php
+// routes/web.php
+use App\Http\Controllers\Admin\RechercheController;
+use App\Http\Controllers\Admin\VulgarisationController;
 
-use Illuminate\Support\Facades\Route;
+Route::prefix('admin')->name('admin.')->middleware(['auth'])->group(function () {
 
-Route::get('/', function () {
-    return view('welcome');
+    Route::resource('recherches', RechercheController::class);
+
+    // Vulgarisations imbriquées dans une recherche
+    Route::prefix('recherches/{recherche}/vulgarisations')->name('vulgarisations.')->group(function () {
+        Route::get('create',        [VulgarisationController::class, 'create'])->name('create');
+        Route::post('/',            [VulgarisationController::class, 'store'])->name('store');
+        Route::delete('/{vulgarisation}', [VulgarisationController::class, 'destroy'])->name('destroy');
+    });
 });
