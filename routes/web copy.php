32b0fa24d0031f9d2f2ffffea42e8c7e819363d2
greya734 <rosajoshu@gmail.com>
2026-06-28 21:43:42 +1100

    auth

--- routes/web copy.php
+++ routes/web copy.php
@@ -1,16 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+
 use App\Http\Controllers\Admin\RechercheController;
 use App\Http\Controllers\Admin\VulgarisationController;
 
-Route::prefix('admin')->name('admin.')->group(function () {
+Route::prefix('admin')->name('admin.')->middleware(['auth'])->group(function () {
 
     Route::resource('recherches', RechercheController::class);
 
+    // Vulgarisations imbriquées dans une recherche
     Route::prefix('recherches/{recherche}/vulgarisations')->name('vulgarisations.')->group(function () {
-        Route::get('create', [VulgarisationController::class, 'create'])->name('create');
-        Route::post('/', [VulgarisationController::class, 'store'])->name('store');
+        Route::get('create',        [VulgarisationController::class, 'create'])->name('create');
+        Route::post('/',            [VulgarisationController::class, 'store'])->name('store');
         Route::delete('/{vulgarisation}', [VulgarisationController::class, 'destroy'])->name('destroy');
     });
 });
