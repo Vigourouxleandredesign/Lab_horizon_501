db2d6f71122a62ab670117c5fbae176dc850b11e
greya734 <rosajoshu@gmail.com>
2026-07-01 07:53:29 +1100

    Update HalImportService.php

--- app/Services/HalImportService.php
+++ app/Services/HalImportService.php
@@ -39,7 +39,7 @@ class HalImportService
 
         if ($domaine === null) {
             $params['fq']   = 'submittedDate_tdate:[NOW-1MONTH TO NOW]';
-            $params['rows'] = 100;
+            $params['rows'] = 1000;
         } else {
             $params['fq'] = 'domain_s:"' . $domaine . '"';
         }
